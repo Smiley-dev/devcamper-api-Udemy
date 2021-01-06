ca39827acdd7d94bbe823e7781f6f43f2ba13f20
SmileyDev <nemanja.gakovic@hotmail.com>
2021-01-06 12:34:13 +0100

    Bootcamp controller and Bootcamp model

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-//Route files
-const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 //Load config files
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+//Body Parser
+app.use(express.json());
+
+//Dev logging Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routes
 app.use("/api/v1/bootcamps", bootcamps);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () =>
+const server = app.listen(port, () =>
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${port} `,
+    `Server running in ${process.env.NODE_ENV} mode on port ${port} `.yellow
+      .bold,
   ),
 );
+
+//Handle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server and exit proccess
+  server.close(() => process.exit(1));
+});
