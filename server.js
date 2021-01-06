57c3519caa113f37fd4e9484f2251d6c5c8436a9
SmileyDev <nemanja.gakovic@hotmail.com>
2021-01-06 10:47:07 +0100

    Initial Routes and Controllers

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
 //Load config files
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Mount routes
+app.use("/api/v1/bootcamps", bootcamps);
 
 const port = process.env.PORT || 5000;
 
